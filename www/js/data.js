8648c8e80d3208cfc1a91200b5f382b83923155c
Nilam Doctor <nilamdoc@gmail.com>
2015-04-10 11:12:42 +0530

    7

--- www/js/data.js
+++ www/js/data.js
@@ -27,7 +27,6 @@ function readDB(){
 			var id = rowsArray[i].id;
 			var address = rowsArray[i].address;
 			// qrcode
-			var qrCode = qr_code.qrcode(3, 'L');
 //			var address = $(this).attr('data-address');
 			var text = address.replace(/^[\s\u3000]+|[\s\u3000]+$/g, '');
 			qrCode.addData(text);
